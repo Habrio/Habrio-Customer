effc4ad3e213ca2fc9b3b1a6adf0c2c8632ba3b0
Habrio <habrioapp@gmail.com>
2025-07-23 08:05:51 +0000

    commit 8

--- src/router.jsx
+++ src/router.jsx
@@ -1,50 +1,23 @@
 // src/router.jsx
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-import TitleScreen from './pages/TitleScreen';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import TitleScreen from './pages/TitleScreen';
 import Otp from './pages/Otp';
 import BasicOnboarding from './pages/BasicOnboarding';
 import ConsumerOnboarding from './pages/ConsumerOnboarding';
-import Home from './pages/Home';
 import ShopList from './pages/ShopList';
-// Future imports like: ShopView, Orders, Cart, Profile, etc.
 
-
-// 🔐 Token-based access protection
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('auth_token');
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default function AppRouter() {
+export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<TitleScreen />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/otp" element={<Otp />} />
-
-        {/* Onboarding routes (requires token) */}
-        <Route path="/onboarding/basic" element={
-          <PrivateRoute><BasicOnboarding /></PrivateRoute>
-        } />
-        <Route path="/onboarding/consumer" element={
-          <PrivateRoute><ConsumerOnboarding /></PrivateRoute>
-        } />
-
-        {/* Authenticated user routes */}
-        <Route path="/home" element={
-          <PrivateRoute><Home /></PrivateRoute>
-        } />
-        <Route path="/shops" element={
-          <PrivateRoute><ShopList /></PrivateRoute>
-        } />
-
-        {/* Optional: fallback */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<TitleScreen />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/otp" element={<Otp />} />
+      <Route path="/onboarding/basic" element={<BasicOnboarding />} />
+      <Route path="/onboarding/consumer" element={<ConsumerOnboarding />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/shops" element={<ShopList />} />
+    </Routes>
   );
 }
